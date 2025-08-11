9de044e4fc8b796f184e87503a966c1fa2b83de6
James Kamau <jamesnk5773@gmail.com>
2025-08-11 20:08:50 +0300

    fibbonacci sequemce

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    fibonacci_sequence = []
+    for i in range (length):
+        if i == 0:
+            fibonacci_sequence.append(0)
+            
+        elif i == 1:
+            fibonacci_sequence.append(1)
+        else:
+            next_num =  fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_num)       
+    print(fibonacci_sequence)        
